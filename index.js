4eaf7f39e260c68dc8a78e9f81a919a8db27e757
Mattazz <matthewazada101@gmail.com>
2024-04-03 15:19:54 -0400

    books html finally working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import pg from 'pg'
+import axios from 'axios'
 
 // server details
 const app = express()
@@ -12,20 +13,67 @@ const db = new pg.Client({
     user: 'postgres',
     host: 'localhost',
     database: 'mattazada',
-    password: '1234',
+    password: '1234', 
     port: 5432,
 })
 db.connect()
 // database details end////////
 ///////////////////////////////
 
+/// book object to store in database
+
+class Book{
+    constructor(title, author, ratings_average, isbn){
+        this.title = title;
+        this.author = author;
+        this.ratings_average = ratings_average;
+        this.isbn = isbn;
+
+    }
+}
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
 
+app.get('/', async (req, res) =>{
+    const result = await db.query('SELECT * from books')
+    let books = []
+    result.rows.forEach((book) =>{
+        books.push(book)
+        // console.log(book);
+    })
+    res.render('index.ejs', {books: books})
+})
 
-app.get('/', (req, res) =>{
-    res.render('index.ejs', {database: db.database})
+app.get('/search', async (req, res) => {
+    const user_search = req.query.searchTitle
+    
+    let openLib_search = `https://openlibrary.org/search.json?title=${user_search}`
+
+    try {
+        const response = await axios.get(openLib_search);
+        const data = response.data;
+    
+        if (data.docs && data.docs[0] && data.docs[0].isbn) {
+            const firstBook = data.docs[0];
+
+            console.log(firstBook.title_suggest, firstBook.author_name[0]);
+            // Create a new Book object with the data from the first book
+            let user_book = new Book(firstBook.title_suggest, firstBook.author_name[0], firstBook.first_publish_year, firstBook.isbn[0]);
+            // add to DB
+            let sql = 'INSERT INTO books (title, author, year, isbn) VALUES ($1, $2, $3, $4)';
+            let values = [user_book.title, user_book.author, user_book.first_publish_year, user_book.isbn]
+            await db.query( sql, values, (err, result) =>{
+                if (err) throw err;
+                console.log("Book added to database");
+            })
+
+            res.redirect('/')
+        }
+    } catch(error){
+        console.error(`Error: ${error}`);
+    }
+
 })
 
 app.listen(port, ()=>{
