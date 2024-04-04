d165825c3493efdf14a52cf9cca1629397bd0112
Mattazz <matthewazada101@gmail.com>
2024-04-04 09:17:03 -0400

    added image link into the information

--- index.js
+++ index.js
@@ -21,54 +21,60 @@ db.connect()
 ///////////////////////////////
 
 /// book object to store in database
-
 class Book{
-    constructor(title, author, ratings_average, publish_year, isbn, first_sentence){
+    constructor(title, author, ratings_average, publish_year, isbn, first_sentence, image_link){
         this.title = title;
         this.author = author;
         this.ratings_average = ratings_average;
         this.publish_year = publish_year;
         this.isbn = isbn;
         this.first_sentence = first_sentence;
+        this.image_link = image_link;
     }
 }
 
+// Middleware
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
 
+
 app.get('/', async (req, res) =>{
-    const result = await db.query('SELECT * from books')
+    const result = await db.query('SELECT * from books') // get array of all books in DB
     let books = []
     result.rows.forEach((book) =>{
-        books.push(book)
+        books.push(book) // push each book into the books array
         // console.log(book);
     })
     res.render('index.ejs', {books: books})
 })
 
 app.get('/search', async (req, res) => {
-    const user_search = req.query.searchTitle;
+    const user_search = req.query.searchTitle; // get user searched title
     let openLib_search = `https://openlibrary.org/search.json?title=${user_search}`;
 
     try {
-        const response = await axios.get(openLib_search);
-        const data = response.data;
+        const response = await axios.get(openLib_search); // request info from openLib API
+        const data = response.data; // store
 
-        if (data.docs && data.docs[0]) {
-            const firstBook = data.docs[0];
+        if (data.docs && data.docs[0]) { // check if exists
+            const firstBook = data.docs[0]; // get first book in the array of search results
 
+            // existence check for each column
             let author_name = firstBook.author_name && Array.isArray(firstBook.author_name) ? firstBook.author_name[0] : "";
             let isbn = firstBook.isbn && Array.isArray(firstBook.isbn) ? firstBook.isbn[0] : "";
             let first_sentence = firstBook.first_sentence && Array.isArray(firstBook.first_sentence) ? firstBook.first_sentence[0] : "";
 
-            console.log(firstBook.title_suggest, author_name, firstBook.first_publish_year, first_sentence);
+            // get image using isbn
+            let coverImage_search = `https://covers.openlibrary.org/b/isbn/${firstBook.isbn[0]}-M.jpg`
+            console.log(coverImage_search);
 
-            let user_book = new Book(firstBook.title_suggest, author_name, parseFloat(firstBook.ratings_average), parseInt(firstBook.first_publish_year), isbn, first_sentence);
+            // Store values
+            let user_book = new Book(firstBook.title_suggest, author_name, parseFloat(firstBook.ratings_average), parseInt(firstBook.first_publish_year), isbn, first_sentence, coverImage_search);
 
-            console.log(user_book);
 
-            let sql = 'INSERT INTO books (title, author, ratings_average, publish_year, isbn, first_sentence) VALUES ($1, $2, $3, $4, $5, $6)';
-            let values = [user_book.title, user_book.author, user_book.ratings_average, user_book.publish_year, user_book.isbn, user_book.first_sentence];
+            // SQL query POST
+            let sql = 'INSERT INTO books (title, author, ratings_average, publish_year, isbn, first_sentence, image_link) VALUES ($1, $2, $3, $4, $5, $6, $7)';
+            let values = [user_book.title, user_book.author, user_book.ratings_average, user_book.publish_year, user_book.isbn, user_book.first_sentence, user_book.image_link];
 
             db.query(sql, values, (err, result) => {
                 if (err) throw err;
