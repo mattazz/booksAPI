6d3ce4efeb7fa0fd3c23ae0c62d537c777609fdc
Mattazz <matthewazada101@gmail.com>
2024-04-04 08:39:21 -0400

    added book description

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ db.connect()
 /// book object to store in database
 
 class Book{
-    constructor(title, author, ratings_average, publish_year, isbn){
+    constructor(title, author, ratings_average, publish_year, isbn, first_sentence){
         this.title = title;
         this.author = author;
         this.ratings_average = ratings_average;
         this.publish_year = publish_year;
         this.isbn = isbn;
-
+        this.first_sentence = first_sentence;
     }
 }
 
@@ -47,36 +47,41 @@ app.get('/', async (req, res) =>{
 })
 
 app.get('/search', async (req, res) => {
-    const user_search = req.query.searchTitle
-    
-    let openLib_search = `https://openlibrary.org/search.json?title=${user_search}`
+    const user_search = req.query.searchTitle;
+    let openLib_search = `https://openlibrary.org/search.json?title=${user_search}`;
 
     try {
         const response = await axios.get(openLib_search);
         const data = response.data;
-    
-        if (data.docs && data.docs[0] && data.docs[0].isbn) {
+
+        if (data.docs && data.docs[0]) {
             const firstBook = data.docs[0];
 
-            console.log(firstBook.title_suggest, firstBook.author_name[0], firstBook.first_publish_year);
-            // Create a new Book object with the data from the first book
-            let user_book = new Book(firstBook.title_suggest, firstBook.author_name[0], parseFloat(firstBook.ratings_average),  parseInt(firstBook.first_publish_year), firstBook.isbn[0]);
-            // add to DB
-            let sql = 'INSERT INTO books (title, author, ratings_average, publish_year, isbn) VALUES ($1, $2, $3, $4, $5)';
-            let values = [user_book.title, user_book.author, user_book.ratings_average, user_book.publish_year, user_book.isbn]
-            db.query( sql, values, (err, result) =>{
+            let author_name = firstBook.author_name && Array.isArray(firstBook.author_name) ? firstBook.author_name[0] : "";
+            let isbn = firstBook.isbn && Array.isArray(firstBook.isbn) ? firstBook.isbn[0] : "";
+            let first_sentence = firstBook.first_sentence && Array.isArray(firstBook.first_sentence) ? firstBook.first_sentence[0] : "";
+
+            console.log(firstBook.title_suggest, author_name, firstBook.first_publish_year, first_sentence);
+
+            let user_book = new Book(firstBook.title_suggest, author_name, parseFloat(firstBook.ratings_average), parseInt(firstBook.first_publish_year), isbn, first_sentence);
+
+            console.log(user_book);
+
+            let sql = 'INSERT INTO books (title, author, ratings_average, publish_year, isbn, first_sentence) VALUES ($1, $2, $3, $4, $5, $6)';
+            let values = [user_book.title, user_book.author, user_book.ratings_average, user_book.publish_year, user_book.isbn, user_book.first_sentence];
+
+            db.query(sql, values, (err, result) => {
                 if (err) throw err;
                 console.log("Book added to database");
-            })
+            });
 
-            res.redirect('/')
+            res.redirect('/');
         }
-    } catch(error){
+    } catch (error) {
         console.error(`Error: ${error}`);
     }
-
-})
+});
 
 app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`);
+    console.log(`l81: Server running on port ${port}`);
 })
